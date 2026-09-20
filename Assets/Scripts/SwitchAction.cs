7d948eb618aa72375db4b414d8eac8096531c074
a-oberle <abo84@msstate.edu>
2026-09-20 02:46:37 -0500

    interaction works

--- Assets/Scripts/SwitchAction.cs
+++ Assets/Scripts/SwitchAction.cs
@@ -3,16 +3,56 @@ using UnityEngine;
 public class SwitchAction : MonoBehaviour
 {
     public Animator anim;
+    public Activate activate;
+
+    private bool cooldown = true;
+
+    private void OnEnable()
+    {
+        CheckForExistingCollision();
+    }
+
+    public void CheckForExistingCollision()
+    {
+        BoxCollider box = GetComponent<BoxCollider>();
+
+        Collider[] overlaps = Physics.OverlapBox(
+            box.bounds.center,
+            box.bounds.extents,
+            box.transform.rotation
+        );
+
+        foreach (Collider collider in overlaps)
+        {
+            if (collider.CompareTag("GameController"))
+            {
+                ActivateLever();
+                return;
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("GameController"))
+        if (other.CompareTag("GameController") && cooldown)
         {
-
-            Debug.Log("Switch");
-
-            anim.SetTrigger("Press");
-
-            //START. Bool? 
+            ActivateLever();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("GameController"))
+        {
+            cooldown = true;
+        }
+    }
+
+    private void ActivateLever()
+    {
+        cooldown = false;
+
+        anim.SetTrigger("Press");
+        activate.ActivateAcceptLever();
     }
 }
